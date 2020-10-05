1b08dbacc4e000a71fd95c39806fccf90891d22d
Francisco Arriagada <franciscorarriagada@gmail.com>
2020-10-05 20:35:16 -0300

    Initial Commit

---
+++ iMeow/iMeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMeowApp.swift
+//  iMeow
+//
+//  Created by Francisco Arriagada on 05-10-20.
+//
+
+import SwiftUI
+
+@main
+struct iMeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
